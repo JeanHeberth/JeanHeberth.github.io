83964f20c9a22ca0ead4e657e515c9a77d32add9
jeanheberth <jeanheberth19@gmail.com>
2024-10-17 18:26:14 -0300

    alterando arquivo

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.toggle-btn');
 
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const targetId = button.getAttribute('data-target');
+            const targetCard = document.getElementById(targetId);
+
+            // Alterna entre mostrar ou esconder o card
+            if (targetCard.style.display === "none" || targetCard.style.display === "") {
+                targetCard.style.display = "block";
+            } else {
+                targetCard.style.display = "none";
+            }
+        });
+    });
+});
